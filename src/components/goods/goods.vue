8fc766a01e08bd187fd46a4b4af69943ae70a6ae
june <june@MacBook-Pro-3.local>
2017-07-12 17:29:46 +0800

    饿了吗app 商品组件开发

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,15 +2,38 @@
   <div class="goods">
     <div class="menu-wrapper">
       <ul>
-        <li v-for="item in goods">
-          <span class="text">
-            <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+        <li v-for="item in goods" class="menu-item">
+        <span class="text">
+          <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
     <div class="foods-wrapper">
-      
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -35,7 +58,6 @@
         Object.assign(res, res.body);
         if (res.errno === ERR_OK) {
           this.goods = res.data;
-          console.log(this.goods);
         }
       });
     },
@@ -43,6 +65,9 @@
 </script>
 
 <style lang="stylus" type="text/stylus">
+@import "../../common/stylus/base";
+@import "../../common/stylus/mixin";
+@import "../../common/stylus/icon";
   .goods
     display: flex
     position: absolute
@@ -54,7 +79,87 @@
       flex: 0 0 80px
       width: 80px
       background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        line-height: 14px
+        padding: 0 12px
+        border-bottom: 1px solid rgba(7, 17, 27, .1)
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          font-size: 12px
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        padding-left: 2px solid #d9dde1
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, .1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+          img
+            width: 100%
+            height: 100%
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            font-size: 14px
+            line-height: 14px
+            color: rgb(7, 17, 27)
+            height: 14px
+          .desc, .extra
+            font-size: 10px
+            line-height: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            margin-bottom: 8px
+          .extra
+            &.count
+              marign-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              marign-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 </style>
 
